c4ae82e226ae717f355c0c864949458db97eea05
dhe19 <info.die@gmail.com>
2023-02-19 00:59:16 -0600

    update firebase config

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,9 +3,19 @@ const {initializeApp, applicationDefault} = require('firebase-admin/app');
 const {getFirestore} =require('firebase-admin/firestore');
 const admin = require('firebase-admin');
 
-const serviceAccount = JSON.parse(process.env.GOOGLE_CREDS)
 initializeApp({
-    credential: admin.credential.cert(serviceAccount)
+    credential: admin.credential.cert({
+        type: process.env.TYPE,
+        projectId: process.env.PROJECT_ID,
+        private_key_id: process.env.PRIVATE_KEY_ID,
+        privateKey: process.env.PRIVATE_KEY,
+        clientEmail:process.env.CLIENT_EMAIL,
+        client_id: process.env.CLIENT_ID,
+        auth_uri: process.env.AUTH_URI,
+        token_uri: process.env.TOKEN_URI,
+        auth_provider_x509_cert_url: process.env.AUTH_PROVIDER,
+        client_x509_cert_url: process.env.CLIENT_CERT
+    })
 })
 
 
